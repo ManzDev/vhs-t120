02faa10c54b06b3dd95a99b3866d48809af926e7
ManzDev <JoseRomanHernandez@gmail.com>
2021-03-01 15:43:22 +0000

    Change model

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import "./components/VHSCassetteFilm.js";
 import "./components/VHSCassette.js";
 import "./components/VHSBox.js";
 import "./components/KodakBox.js";
+
+const MODELS = ["vhs", "kodak"];
+let currentIndex = 0;
+
+const container = document.querySelector(".container");
+
+document.body.onclick = () => {
+  currentIndex = (currentIndex + 1) % MODELS.length;
+  const currentModel = MODELS[currentIndex];
+  container.innerHTML = `
+    <${currentModel}-box></${currentModel}-box>
+    <vhs-cassette model="${currentModel}"></vhs-cassette>
+  `;
+};
